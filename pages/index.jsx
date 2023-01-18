241c4e022f195225f22f716169374dc6e14f03c0
divizn <hassanjaved186@gmail.com>
2023-01-18 06:59:43 +0000

    fix: changed url of portfolio items to the updated path

--- pages/index.jsx
+++ pages/index.jsx
@@ -317,35 +317,35 @@ export default function Home() {
                 href="https://github.com/divizn/discord-spotify-bot"
                 target="_blank"
               >
-                <img src="https://github-readme-stats.vercel.app/api/pin/?username=divizn&repo=discord-spotify-bot&hide_border=true&theme=dark&bg_color=0d1117" />
+                <img src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/pin/?username=divizn&repo=discord-spotify-bot&hide_border=true&theme=dark&bg_color=0d1117" />
               </a>
               <a
                 className="w-fit"
                 href="https://github.com/divizn/react-weather-app"
                 target="_blank"
               >
-                <img src="https://github-readme-stats.vercel.app/api/pin/?username=divizn&repo=react-weather-app&hide_border=true&theme=dark&bg_color=0d1117" />
+                <img src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/pin/?username=divizn&repo=react-weather-app&hide_border=true&theme=dark&bg_color=0d1117" />
               </a>
               <a
                 className="w-fit"
                 href="https://github.com/divizn/legacy-portfolio-site"
                 target="_blank"
               >
-                <img src="https://github-readme-stats.vercel.app/api/pin/?username=divizn&repo=legacy-portfolio-site&hide_border=true&theme=dark&bg_color=0d1117" />
+                <img src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/pin/?username=divizn&repo=legacy-portfolio-site&hide_border=true&theme=dark&bg_color=0d1117" />
               </a>
               <a
                 className="w-fit"
                 href="https://github.com/divizn/portfolio-site"
                 target="_blank"
               >
-                <img src="https://github-readme-stats.vercel.app/api/pin/?username=divizn&repo=portfolio-site&hide_border=true&theme=dark&bg_color=0d1117" />
+                <img src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/pin/?username=divizn&repo=portfolio-site&hide_border=true&theme=dark&bg_color=0d1117" />
               </a>
               <a
                 className="w-fit"
                 href="https://github.com/divizn/clone-sites"
                 target="_blank"
               >
-                <img src="https://github-readme-stats.vercel.app/api/pin/?username=divizn&repo=clone-sites&hide_border=true&theme=dark&bg_color=0d1117" />
+                <img src="https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/pin/?username=divizn&repo=clone-sites&hide_border=true&theme=dark&bg_color=0d1117" />
               </a>
             </div>
             <div className="h-screen"></div>
