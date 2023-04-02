c5883d58e19564f52e53acb2c7b8cdb788ec65c1
divizn <hassanjaved186@gmail.com>
2023-04-02 18:15:58 +0100

    add meta tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -38,15 +38,37 @@ export default function Home() {
           name="description"
           content="Hello, my name is Hassan Javed, a software developer and student living in the UK. Here you can see my journey as a software developer and what I have done in the past."
         />
-        <meta content="Hassan Javed - Portfolio" property="og:title" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta
+          name="apple-mobile-web-app-title"
+          content="Hassan Javed - Portfolio"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://hassanj.dev" />
+        <meta name="twitter:title" content="Hassan Javed - Portfolio" />
+        <meta
+          name="twitter:description"
           content="Hello, my name is Hassan Javed, a software developer and student living in the UK. Here you can see my journey as a software developer and what I have done in the past."
-          property="og:description"
         />
         <meta
-          content="https://raw.githubusercontent.com/divizn/portfolio-site/main/public/favicon.ico"
-          property="og:image"
+          name="twitter:image"
+          content="https://avatars.githubusercontent.com/u/92521687?v=4"
         />
+        <meta name="twitter:creator" content="@_divizn" />
+        <meta property="og:title" content="Hassan Javed - Portfolio" />
+        <meta
+          property="og:description"
+          content="Hello, my name is Hassan Javed, a software developer and student living in the UK. Here you can see my journey as a software developer and what I have done in the past."
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/92521687?v=4"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Hassan Javed - Portfolio" />
+        <meta property="og:url" content="https://hassanj.dev" />
         <meta name="robots" content="index,follow" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
