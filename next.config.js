41fed1d3eedb477c41697bd1bd3e433e7e77467c
divizn <hassanjaved186@gmail.com>
2023-04-02 16:19:52 +0100

    adding image domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["github-readme-stats-git-masterrstaa-rickstaa.vercel.app"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
