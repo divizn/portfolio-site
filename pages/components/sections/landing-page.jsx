8b485e3124a82c1f117c83df34f9ad85ba711914
divizn <hassanjaved186@gmail.com>
2023-04-03 19:47:50 +0100

    add mobile only cv button and alt tags

--- pages/components/sections/landing-page.jsx
+++ pages/components/sections/landing-page.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { BsFillMoonStarsFill, BsGithub, BsLinkedin } from "react-icons/bs";
+import {
+  BsCloud,
+  BsCloudArrowUp,
+  BsDownload,
+  BsFilePdf,
+  BsFillMoonStarsFill,
+  BsGithub,
+  BsLinkedin,
+} from "react-icons/bs";
 import { GrMail } from "react-icons/gr";
 
 const Landing = ({ getDark }) => {
@@ -8,14 +16,28 @@ const Landing = ({ getDark }) => {
 
   return (
     <section className="flex h-screen flex-col bg-neutral-50 dark:bg-black">
-      <button className="m-10 flex bg-neutral-50 dark:bg-black">
-        <BsFillMoonStarsFill
-          size={40}
-          className="select-none text-black dark:text-neutral-50"
-          onClick={() => setDarkMode(!darkMode)}
-        />
-      </button>
-      <div className="flex h-full flex-col items-center justify-center text-center text-neutral-700 dark:text-slate-200">
+      <div className="flex justify-between">
+        <button className="my-10 ml-10 flex bg-neutral-50 dark:bg-black">
+          <BsFillMoonStarsFill
+            size={40}
+            className="select-none text-black dark:text-neutral-50"
+            onClick={() => setDarkMode(!darkMode)}
+          />
+        </button>
+        <a
+          href="https://drive.google.com/file/d/1C3KaFUDf4j-14dhSrZ8x-yDbq_JW0gVz/view?usp=sharing"
+          rel="noreferrer noopener"
+          target="_blank"
+          className="m-10 flex select-none items-center bg-neutral-50 font-medium text-neutral-700 dark:bg-black dark:text-slate-200 sm:hidden"
+        >
+          CV/Resume
+          <BsFilePdf
+            size={40}
+            className="ml-2 select-none text-black dark:text-neutral-50"
+          />
+        </a>
+      </div>
+      <div className="flex h-full flex-col items-center justify-center p-[5%] text-center text-neutral-700 dark:text-slate-200">
         <h2 className="py-2 text-5xl font-medium">
           Hi, I am{" "}
           <span className="text-blue-600 dark:text-lime-500">Hassan Javed</span>
