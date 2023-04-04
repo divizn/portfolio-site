c0bb0a5da7a5c8d509e755317fc80bfaba269c84
divizn <hassanjaved186@gmail.com>
2023-04-04 15:27:43 +0100

    update description

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -11,8 +11,16 @@ const Description = () => {
           My first ever programming language was Lua on ROBLOX in 2010. 2 years
           later, I was introduced to Scratch and then Python. I have been
           constantly working on my skills and am now pursuing my passion by
-          studying Computer Science in university. Check out my CV to see more
-          skills I have developed on my journey.
+          studying Computer Science in university. Check out my{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-semibold text-blue-600 underline dark:text-lime-500"
+            href="https://drive.google.com/file/d/1C3KaFUDf4j-14dhSrZ8x-yDbq_JW0gVz/view?usp=sharing"
+          >
+            CV
+          </a>{" "}
+          to see more skills I have developed on my journey.
         </p>
         <h3 className="py-3 text-center text-3xl font-semibold text-blue-600  dark:text-lime-500">
           Present
@@ -20,9 +28,25 @@ const Description = () => {
         <p className="text-center text-neutral-700 dark:text-slate-200">
           I am constantly working on my skills and I love watching videos online
           of new things that I have never learnt before. Currently, I am
-          learning front end web development using technologies such as
-          JavaScript, React, TailwindCSS, Node.JS and I am also trying out
-          Next.JS.
+          learning backend web development using technologies such as{" "}
+          <span className="font-semibold text-blue-600 dark:text-lime-500">
+            JavaScript
+          </span>{" "}
+          (a bit of{" "}
+          <span className="font-semibold text-blue-600 dark:text-lime-500">
+            TypeScript
+          </span>{" "}
+          too) ,{" "}
+          <span className="font-semibold text-blue-600 dark:text-lime-500">
+            NodeJs
+          </span>
+          ,
+          <span className="font-semibold text-blue-600 dark:text-lime-500"></span>{" "}
+          and{" "}
+          <span className="font-semibold text-blue-600 dark:text-lime-500">
+            NextJS
+          </span>
+          .
         </p>
         <h3 className="py-3 text-center text-3xl font-semibold text-blue-600 dark:text-lime-500">
           Future
