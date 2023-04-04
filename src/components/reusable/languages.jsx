c497d7f5fa9acb31443c66b41616728fd53d5e0b
divizn <hassanjaved186@gmail.com>
2023-04-04 04:45:22 +0100

    temp fix for language colors

--- src/components/reusable/languages.jsx
+++ src/components/reusable/languages.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
-// TODO: fix the progress bar, find out why w-[${progress}%] doesn't work so I can remove glabal.css styles
+// TODO: fix the progress bar, find out why w-[${progress}%] doesn't work so I can remove glabal.css styles --- SORT OF FIXED: template strings + props dont work
 
-const Languages = ({ icon, progress, name, color }) => {
+const Languages = ({ icon, progress, name }) => {
+  const variants = {
+    nextjs: "text-neutral-700 dark:text-slate-200",
+    reactjs: "text-blue-500",
+    tailwindcss: "text-cyan-600",
+    clang: "text-blue-500",
+    javascript: "text-yellow-400",
+    lua: "text-blue-500",
+    python: "text-yellow-500",
+    html: "text-red-500",
+    css: "text-blue-500",
+    java: "text-orange-500",
+    csharp: "text-blue-500",
+    typescript: "text-blue-500",
+  };
+  const progBar = `w-${progress}%`;
   return (
     <div className="flex flex-row">
-      <p className={`py-3 text-7xl ${color}`}>{icon}</p>
+      <p className={`${variants[name]} py-3 text-7xl`}>{icon}</p>
       <p className="mx-3 self-center text-neutral-700 dark:text-slate-200 ">
         {progress}%
       </p>
--- src/lib/languages.js
+++ src/lib/languages.js
@@ -17,31 +17,21 @@ export const currentLanguages = [
   {
     icon: <SiJavascript />,
     name: "javascript",
-    color: "text-yellow-400",
     progress: 90,
   },
   {
     icon: <SiReact />,
     name: "reactjs",
-    color: "text-cyan-500",
     progress: 85,
-  },
-  {
-    icon: <SiTailwindcss />,
-    name: "tailwindcss",
-    color: "text-cyan-600",
-    progress: 79,
   },
   {
     icon: <SiNextdotjs />,
     name: "nextjs",
-    color: "text-neutral-300",
-    progress: 55,
+    progress: 80,
   },
   {
     icon: <SiC />,
     name: "clang",
-    color: "text-blue-600",
     progress: 10,
   },
 ];
@@ -50,43 +40,36 @@ export const otherLanguages = [
   {
     icon: <SiJava />,
     name: "java",
-    color: "text-blue-900",
     progress: 90,
-  },
-  {
-    icon: <SiSpring />,
-    name: "spring",
-    color: "text-lime-500",
-    progress: 50,
   },
   {
     icon: <SiCsharp />,
     name: "csharp",
-    color: "text-cyan-600",
     progress: 80,
   },
   {
     icon: <SiPython />,
     name: "python",
-    color: "text-yellow-300",
     progress: 90,
   },
   {
     icon: <SiHtml5 />,
     name: "html",
-    color: "text-red-600",
     progress: 100,
   },
   {
     icon: <SiCss3 />,
     name: "css",
-    color: "text-blue-600",
+    progress: 100,
+  },
+  {
+    icon: <SiTailwindcss />,
+    name: "tailwindcss",
     progress: 100,
   },
   {
     icon: <SiLua />,
     name: "lua",
-    color: "text-blue-600",
     progress: 65,
   },
 ];
